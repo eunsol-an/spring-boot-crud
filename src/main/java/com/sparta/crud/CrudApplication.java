a5ae6b75575a3580df9481b149b8f6069733aeea
eunsol-an <dev.eunsol@gmail.com>
2022-11-29 17:41:10 +0900

    initial commit

---
+++ src/main/java/com/sparta/crud/CrudApplication.java
@@ -0,0 +1,16 @@
+package com.sparta.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class CrudApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CrudApplication.class, args);
+    }
+
+}
