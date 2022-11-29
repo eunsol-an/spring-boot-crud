bf5b28a4db45ab01e520f4f981092d6f7ebca679
eunsol-an <dev.eunsol@gmail.com>
2022-11-30 01:45:29 +0900

    feat: 게시글 수정, 삭제 시 비밀번호 일치 확인

--- src/main/java/com/sparta/crud/controller/BoardController.java
+++ src/main/java/com/sparta/crud/controller/BoardController.java
@@ -7,41 +7,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
     // 게시글 작성
-    @PostMapping("/api/board")
+    @PostMapping("/board")
     public BoardResponseDto createBorad(@RequestBody BoardRequestDto requestDto) {
         return boardService.createBoard(requestDto);
     }
 
     // 전체 게시글 조회
-    @GetMapping("/api/board")
+    @GetMapping("/board")
     public List<BoardResponseDto> getBoardList() {
         return boardService.getBoardList();
     }
 
     // 게시글 상세 조회
-    @GetMapping("/api/board/{id}")
+    @GetMapping("/board/{id}")
     public BoardResponseDto getBoard(@PathVariable Long id) {
         return boardService.getBoard(id);
     }
 
     // 게시글 수정
-    @PutMapping("/api/board/{id}")
+    @PutMapping("/board/{id}")
     public BoardResponseDto updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto) {
         return boardService.update(id, requestDto);
     }
 
     // 게시글 삭제
-    @DeleteMapping("/api/board/{id}")
-    public Map<String, Object> deleteBoard(@PathVariable Long id) {
-        return boardService.deleteBoard(id);
+    @DeleteMapping("/board/{id}")
+    public BoardResponseDto deleteBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto) {
+        return boardService.deleteBoard(id, requestDto);
     }
 }
